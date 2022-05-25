5116d4345c9c9bd8d6a936d633104c4123a395e8
Doruk <73473236+adoruksen@users.noreply.github.com>
2022-05-26 00:30:35 +0300

    Working On Editor Tools
    -Making Helper Utilities for checking if there is an error.
    -Creating RoomNodeTypeListSO

--- Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomNodeTypeSO : MonoBehaviour
+[CreateAssetMenu(fileName ="RoomNodeType_",menuName ="Scriptable Objects/Dungeon/Room Node Type")]
+public class RoomNodeTypeSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string roomNodeTypeName;
 
-    // Update is called once per frame
-    void Update()
+    #region Header
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+    #endregion
+    public bool displayInNodeGraphEditor = true;
+
+    #region Header
+    [Header("One Type Should be A Corridor")]
+    #endregion
+    public bool isCorridor;
+
+    #region Header
+    [Header("One Type Should Be a CorridorNS")]
+    #endregion
+    public bool isCorridorNS;
+
+    #region Header
+    [Header("One Type Should Be a CorridorEW")]
+    #endregion
+    public bool isCorridorEW;
+
+    #region Header
+    [Header("One Type Should Be An Entrance")]
+    #endregion
+    public bool isEntrance;
+
+    #region Header
+    [Header("One Type Should Be a Boss Room")]
+    #endregion
+    public bool isBossRoom;
+
+    #region Header
+    [Header("One Type Should Be None {Unassigned}")]
+    #endregion
+    public bool isNone;
+
+    #region Validation
+#if UNITY_EDITOR // <= Be sure that OnValidate Function only run in the editor.
+
+    //OnValidate method is a special method of MonoBehaviour. We using this to detech changes in the inspector. As I update a value in a scriptable object
+    //this method will get called
+    private void OnValidate()
     {
-        
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(roomNodeTypeName), roomNodeTypeName);
+        //nameof function will return a string of the field name
     }
+#endif
+    #endregion
 }
