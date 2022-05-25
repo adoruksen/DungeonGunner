eb659c3fcbf461c888f2a5dc0236c86695917b2f
Doruk <73473236+adoruksen@users.noreply.github.com>
2022-05-26 00:50:28 +0300

    Working on Editor Tools
    -room node types are created.

--- Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenuAttribute(fileName ="RoomNodeGraph",menuName ="ScriptableObjects/Dungeon/Room Node Graph")]
+[CreateAssetMenuAttribute(fileName ="RoomNodeGraph",menuName ="Scriptable Objects/Dungeon/Room Node Graph")]
 public class RoomNodeGraphSO : ScriptableObject
 {
     [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
